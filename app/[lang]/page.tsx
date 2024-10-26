56a057da610897c4602802a6badc00aacb91c7fc
dieptv <diep.tv1999@gmail.com>
2024-10-26 15:41:13 +0700

    init

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -17,34 +17,6 @@ import React from "react";
 
 export const runtime = "edge";
 
-export const metadata = {
-  title: "Giải pháp quay video đóng hàng thương mại điện tử",
-  description: "Giải pháp quay video đóng hàng thương mại điện tử",
-  openGraph: {
-    type: "website",
-    url: "https://github.com/nobruf/shadcn-landing-page.git",
-    title: "Giải pháp quay video đóng hàng thương mại điện tử",
-    description: "Giải pháp quay video đóng hàng thương mại điện tử",
-    images: [
-      {
-        url: "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
-        width: 1200,
-        height: 630,
-        alt: "Giải pháp quay video đóng hàng thương mại điện tử",
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    site: "/preview.jpg",
-    title: "Giải pháp quay video đóng hàng thương mại điện tử",
-    description: "Giải pháp quay video đóng hàng thương mại điện tử",
-    images: [
-      "https://res.cloudinary.com/dbzv9xfjp/image/upload/v1723499276/og-images/shadcn-vue.jpg",
-    ],
-  },
-};
-
 type Props = {
   params: {
     lang: Locale;
