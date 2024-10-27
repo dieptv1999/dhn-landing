9bf79f5a5de7da4675d16968cb0ca2d577443d1a
dieptv <diep.tv1999@gmail.com>
2024-10-27 22:58:31 +0700

    init

--- components/layout/sections/banner-center.tsx
+++ components/layout/sections/banner-center.tsx
@@ -4,7 +4,7 @@ export default function BannerCenterSection() {
     return (
         <section id="banner-center" className="container py-24 sm:py-32 flex justify-center">
             <div className={'rounded-lg overflow-hidden'}>
-                <Image src={'/sec2.jpeg'} alt={'banner 2'} width={1080} height={412} quality={100}
+                <Image src={'/sec2.jpeg?v=1'} alt={'banner 2'} width={1080} height={412} quality={100}
                        className={'object-cover'}/>
             </div>
         </section>
